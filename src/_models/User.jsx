ee543e084dc30754b2e5af6a34f4b763f605f44d
Jallouli-Yassine <jungloul1@gmail.com>
2024-02-27 22:57:15 +0100

    add user class

---
+++ src/_models/User.jsx
@@ -0,0 +1,10 @@
+class User {
+    constructor() {
+      this.username = '';
+      this.email = '';
+      this.password = '';
+      this.age = 18;
+      this.createdat = new Date();
+    }
+  }
+  export default User;

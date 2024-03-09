bf7f06c79b74774da79882b222ddc5e38488ea04
Jallouli-Yassine <jungloul1@gmail.com>
2024-03-09 19:29:03 +0100

    fix bug

--- src/_shared/protectRoutes.jsx
+++ src/_shared/protectRoutes.jsx
@@ -9,5 +9,15 @@ export const PrivateRoute = () => {
 
 export const DontLogin = () => {
     const currentUser = useSelector(selectUserObject);
+    //console.log("userr : " + currentUser.firstName)
     return currentUser ? <Navigate to="/Home/dashboard" /> : <Outlet/> ;
 };
+/*
+const currentUser = useSelector(selectUserObject);
+if(currentUser && currentUser.settings.verifiedAccount===true){
+    <Navigate to="/Home/dashboard" />
+
+}else {
+    <Outlet/>
+}
+*/

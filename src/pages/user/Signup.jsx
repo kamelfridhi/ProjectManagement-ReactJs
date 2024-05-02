c5d240ae648e7c14e1330e8766aec82c8ab8a955
Jallouli-Yassine <jungloul1@gmail.com>
2024-05-02 18:30:15 +0100

    some features

--- src/pages/user/Signup.jsx
+++ src/pages/user/Signup.jsx
@@ -173,6 +173,8 @@ export default function Signup() {
                                                     <option value="employee">Employee</option>
                                                     <option value="projectManager">Project Manager</option>
                                                     <option value="client">Client</option>
+                                                    <option value="marketing">marketing</option>
+                                                    <option value="developement">developement</option>
                                                 </select>
                                             </div>
                                         </div>

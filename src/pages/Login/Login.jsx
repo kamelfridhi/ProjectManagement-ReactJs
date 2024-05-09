77ea04c10f1ee2f9345b5455bff6a935babc13cb
jallouli yassine <jungloul1@gmail.com>
2024-05-09 22:07:31 +0200

    login responsive

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -75,59 +75,46 @@ try {
 
     return (
         <>
-            <ToastContainer />
-            {/* Mirrored from pixelwibes.com/template/my-task/html/dist/ui-elements/auth-signin.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 12 Feb 2024 11:39:00 GMT */}
-            <meta charSet="utf-8" />
-            <meta httpEquiv="X-UA-Compatible" content="IE=Edge" />
-            <meta
-                name="viewport"
-                content="width=device-width, initial-scale=1, shrink-to-fit=no"
-            />
-            <title>:: TeamSphere:: Signin</title>
-            <link rel="icon" href="../favicon.ico" type="image/x-icon" /> {/* Favicon*/}
-            {/* project css file  */}
-            <link rel="stylesheet" href="/assets/css/my-task.style.min.css" />
             <div id="mytask-layout">
-                {/* main body area */}
-
-                <div className="main p-2 py-3 p-xl-5 ">
-                    {/* Body: Body */}
-                    <div className="body d-flex p-0 p-xl-5" style={{ marginLeft: "300px" }}>
+                <div className="main p-2 py-3 p-xl-5">
+                    <div className="body p-0 p-xl-5">
                         <div className="container-xxl">
                             <div className="row g-0">
-                                <div className="col-lg-6 d-none d-lg-flex justify-content-center align-items-center rounded-lg auth-h100">
+                                <div className="col-lg-6 col-sm d-flex justify-content-center align-items-center rounded-lg auth-h100">
                                     <div style={{ maxWidth: "25rem" }}>
                                         <div className="text-center mb-5">
-                                            <img src="/assets/images/logots.png" alt="logo" style={{width: "150px" , height: "150px" }}/>
+                                            <img
+                                                src="/assets/images/logots.png"
+                                                alt="logo"
+                                                className="img-fluid"
+                                                style={{ width: "150px", height: "150px" }}
+                                            />
                                         </div>
                                         <div className="mb-5">
                                             <h2 className="color-900 text-center">
                                                 TeamSphere Lets Management Better
                                             </h2>
                                         </div>
-                                        {/* Image block */}
-                                        <div className="">
-                                            <img src="/assets/images/login-img.svg" alt="login-img" />
+                                        <div className="text-center">
+                                            <img
+                                                src="/assets/images/login-img.svg"
+                                                alt="login-img"
+                                                className="img-fluid"
+                                            />
                                         </div>
                                     </div>
                                 </div>
 
-                                <div className="col-lg-6 d-flex justify-content-center align-items-center border-0 rounded-lg auth-h100" style={{ backgroundColor: "#4c3575" }}>
-                                    <div
-                                        className="w-100 p-3 p-md-5 card border-0 bg-dark text-light"
-                                        style={{maxWidth: "32rem"}}
-                                    >
+                                <div className="col-lg-6 col-sm d-flex justify-content-center align-items-center rounded-lg auth-h100" style={{ backgroundColor: "#4c3575" }}>
+                                    <div className="w-100 p-3 p-md-5 card border-0 bg-dark text-light" style={{ maxWidth: "90%" }}>
                                         <div className="col-12 text-center mb-1 mb-lg-5">
                                             <h1>Sign in</h1>
                                             <span>Free access to our dashboard.</span>
                                         </div>
                                         <div className="col-12 text-center mb-4">
-                                            <OAuth/>
-                                            <span className="dividers text-muted mt-4">OR</span>
+                                            {/* OAuth component */}
                                         </div>
-                                        {/* Form */}
                                         <form onSubmit={handleSubmit} className="row g-1 p-3 p-md-4">
-
                                             <div className="col-12">
                                                 <div className="mb-2">
                                                     <label className="form-label">Email address</label>
@@ -136,32 +123,34 @@ try {
                                                         onChange={handleChange}
                                                         className="form-control form-control-lg"
                                                         placeholder="name@example.com"
-                                                        id='email'
-
+                                                        id="email"
                                                     />
                                                 </div>
                                             </div>
-                                            {error && error.email && <div className="text-danger">{error.email}</div>}
-
+                                            <div className="col-12">
+                                                {error && error.email && (
+                                                    <div className="text-danger">{error.email}</div>
+                                                )}
+                                            </div>
                                             <div className="col-12">
                                                 <div className="mb-2">
                                                     <div className="form-label">
-                                                        <span
-                                                            className="d-flex justify-content-between align-items-center">
-                                                            Password
-                                                            <Link
-                                                                className="text-secondary"
-                                                                to="change-password"
-                                                            >
-                                                                Forgot Password?
-                                                            </Link>
-                                                        </span>
+                            <span className="d-flex justify-content-between align-items-center">
+                              Password
+                              <a
+                                  className="text-secondary"
+                                  href="change-password"
+                              >
+                                Forgot Password?
+                              </a>
+                            </span>
                                                     </div>
                                                     <input
-                                                        type="password" onChange={handleChange}
+                                                        type="password"
+                                                        onChange={handleChange}
                                                         className="form-control form-control-lg"
                                                         placeholder="***************"
-                                                        id='password'
+                                                        id="password"
                                                     />
                                                 </div>
                                             </div>
@@ -185,37 +174,28 @@ try {
                                                 <button
                                                     disabled={loading}
                                                     type="submit"
-                                                    id='submit'
+                                                    id="submit"
                                                     className="btn btn-lg btn-block btn-light lift text-uppercase"
                                                 >
                                                     {loading ? 'Loading...' : 'Sign in'}
                                                 </button>
-
-
                                             </div>
                                             <div className="col-12 text-center mt-4">
-                                                <span className="text-muted">
-                                                    Dont have an account yet?{" "}
-                                                    <Link to="signup" className="text-secondary">
-                                                        Sign up here
-                                                    </Link>
-                                                </span>
-
+                        <span className="text-muted">
+                          Don't have an account yet?{' '}
+                            <a href="signup" className="text-secondary">
+                            Sign up here
+                          </a>
+                        </span>
                                             </div>
                                         </form>
-                                        {/* End Form */}
                                     </div>
                                 </div>
                             </div>
-                            {" "}
-                            {/* End Row */}
                         </div>
                     </div>
                 </div>
             </div>
-            {/* Jquery Core Js */}
-            {/* Mirrored from pixelwibes.com/template/my-task/html/dist/ui-elements/auth-signin.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 12 Feb 2024 11:39:00 GMT */}
         </>
-
     );
-}
+};

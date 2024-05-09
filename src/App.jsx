24ff7b14de3fa290595b24f9448616ff007ccf3d
jallouli yassine <jungloul1@gmail.com>
2024-05-09 23:00:42 +0200

    remove sidebar resp

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,15 @@
-import React, { useState } from "react";
-import "./App.css";
+import React from 'react'
+
+import './App.css'
 import AppRouter from "./routes/AppRouter.jsx";
-import Sidebar from "./_shared/sidebar/Sidebar.jsx";
 
 function App() {
-  const [showSidebar, setShowSidebar] = useState(false);
-
-  const toggleSidebar = () => {
-    setShowSidebar(!showSidebar);
-  };
-
-  const isAndroidMobile = () => {
-    const userAgent = navigator.userAgent.toLowerCase();
-    return /android/.test(userAgent) && /mobile/.test(userAgent);
-  };
 
   return (
-      <div>
-        {isAndroidMobile() && (
-            <button onClick={toggleSidebar}>Toggle Sidebar</button>
-        )}
+    <AppRouter></AppRouter>
 
-        {showSidebar && <Sidebar />}
-        <AppRouter />
-      </div>
   );
+
 }
 
-export default App;
+export default App

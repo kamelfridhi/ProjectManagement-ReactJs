2e421df50d0a23444bf54d92cf63176a46493b08
jallouli yassine <jungloul1@gmail.com>
2024-05-09 22:58:06 +0200

    sidebar resp

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
-
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
 import AppRouter from "./routes/AppRouter.jsx";
+import Sidebar from "./_shared/sidebar/Sidebar.jsx";
 
 function App() {
+  const [showSidebar, setShowSidebar] = useState(false);
+
+  const toggleSidebar = () => {
+    setShowSidebar(!showSidebar);
+  };
+
+  const isAndroidMobile = () => {
+    const userAgent = navigator.userAgent.toLowerCase();
+    return /android/.test(userAgent) && /mobile/.test(userAgent);
+  };
 
   return (
-    <AppRouter></AppRouter>
+      <div>
+        {isAndroidMobile() && (
+            <button onClick={toggleSidebar}>Toggle Sidebar</button>
+        )}
 
+        {showSidebar && <Sidebar />}
+        <AppRouter />
+      </div>
   );
-
 }
 
-export default App
+export default App;
